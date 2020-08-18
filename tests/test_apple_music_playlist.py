f0eb84634daa1c7ddf91cb3fe8f19a479b9bc739
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-08-17 21:37:38 -0400

    refactored api function

--- tests/test_apple_music_playlist.py
+++ tests/test_apple_music_playlist.py
@@ -1,7 +1,7 @@
-from app.api.apple_music_playlist import AppleMusicPlaylist
+from app.api.apple_music_playlist import AppleMusicPlaylistParser
 from tests.constant_playlist_urls import *
 
-url = AppleMusicPlaylist(APPLE_MUSIC_PLAYLIST_URL_1)
+url = AppleMusicPlaylistParser(APPLE_MUSIC_PLAYLIST_URL_1)
 
 print("Apple Music Playlist URL: " + url.playlist_url)
 print("Storefront: " + url.storefront.upper())
