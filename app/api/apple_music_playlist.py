8a871b3db7b585580c3fdc8682d2c3cab86fcf89
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-08-17 21:36:57 -0400

    added documentation

--- app/api/apple_music_playlist.py
+++ app/api/apple_music_playlist.py
@@ -1,8 +1,27 @@
-from . import Song
+class AppleMusicPlaylistParser:
+    """
+    A class used to parse a playlist URL from Apple Music
 
+    Attributes
+    ----------
+    playlist_url : str
+        The Apple Music Playlist URL
+    storefront : str
+        The Apple Music Storefront for the playlist
+    playlist_id : str
+        The Apple Music Playlist ID for the playlist
 
-class AppleMusicPlaylist:
+    Methods
+    -------
+
+    """
     def __init__(self, url):
+        """
+        Parameters
+        ----------
+        url : str
+            The Apple Music Playlist URL
+        """
         self.playlist_url = url
         self.storefront = self.get_storefront()
         self.playlist_id = self.get_playlist_id()
@@ -15,9 +34,27 @@ class AppleMusicPlaylist:
         # }
 
     def get_storefront(self):
+        """Parses the playlist URL to determine the storefront.
+
+        The storefront is necessary for accessing Apple Music's API endpoint.
+
+        Returns
+        -------
+        storefront : str
+            A two-character string that represents the storefront
+        """
         url = self.playlist_url
         return url[url.find("apple.com/") + 10:url.find("/playlist")]
 
     def get_playlist_id(self):
+        """Parses the playlist URL to determine the playlist ID.
+
+        The playlist ID is necessary for accessing Apple Music's API endpoint.
+
+        Returns
+        -------
+        playlist_id : str
+            The playlist ID
+        """
         url = self.playlist_url
         return url[url.rfind("/") + 1:]
