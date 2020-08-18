1ccc816491f52da8878d9ad5bde68b7b83aade1f
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-08-17 21:19:29 -0400

    added documentation

--- app/api/song.py
+++ app/api/song.py
@@ -5,8 +5,30 @@ class Song:
     ...
 
     Attributes
+    ----------
+    name : str
+        The name of the song
+    artist : str
+        The artist of the song
+    album : str
+        The album in which the song is included
+    isrc : str
+        The Internation Standard Recording Code (ISRC) for the song
     """
+
     def __init(self, name, artist, album, isrc):
+        """
+        Parameters
+        ----------
+        name : str
+            The name of the song
+        artist : str
+            The artist of the song
+        album : str
+            The album in which the song is included
+        isrc : str
+            The Internation Standard Recording Code (ISRC) for the song
+        """
         self.name = name
         self.artist = artist
         self.album = album
