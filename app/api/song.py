4074fa49ed00baf158a3c15c7d7acf97ecd06c7c
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-08-18 22:56:52 -0400

    modified class to not take any parameters

--- app/api/song.py
+++ app/api/song.py
@@ -16,20 +16,8 @@ class Song:
         The Internation Standard Recording Code (ISRC) for the song
     """
 
-    def __init(self, name, artist, album, isrc):
-        """
-        Parameters
-        ----------
-        name : str
-            The name of the song
-        artist : str
-            The artist of the song
-        album : str
-            The album in which the song is included
-        isrc : str
-            The Internation Standard Recording Code (ISRC) for the song
-        """
-        self.name = name
-        self.artist = artist
-        self.album = album
-        self.isrc = isrc
+    def __init__(self):
+        self.name = ""
+        self.artist = ""
+        self.album = ""
+        self.isrc = ""
