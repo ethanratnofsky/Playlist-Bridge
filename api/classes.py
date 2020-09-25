955d2f7d1586aafa9069cda428302ec7e192d5dc
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-09-24 22:59:00 -0400

    created Song and Playlist classes

---
+++ api/classes.py
@@ -0,0 +1,13 @@
+class Song:
+    def __init__(self):
+        self.title = ''
+        self.artist = ''
+        self.isrc = None
+
+
+class Playlist:
+    def __init__(self):
+        self.title = ''
+        self.description = ''
+        self.creator = ''
+        self.songs = []
