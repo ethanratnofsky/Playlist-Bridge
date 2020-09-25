6bf10652b57512c4f887627fe9b853e13fd975d5
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-09-25 00:32:36 -0400

    added parse/create methods to function calls; created tidal_parser file

--- api/bridger.py
+++ api/bridger.py
@@ -1,12 +1,12 @@
 def bridge(src, dest, playlist_url):
     if src == 'tidal':
-        playlist = tidal_parser(playlist_url)
+        playlist = tidal_parser.parse(playlist_url)
     # elif src == 'spotify':
-    #     playlist = spotify_parser(playlist_url)
+    #     playlist = spotify_parser.parse(playlist_url)
     # elif src == 'apple_music':
-    #     playlist = apple_music_parser(playlist_url)
+    #     playlist = apple_music_parser.parse(playlist_url)
     #
     # if dest == 'spotify':
-    #     print(spotify_creator(playlist))
+    #     print(spotify_creator.create(playlist))
     # elif dest == 'apple_music':
-    #     print(apple_music_creator(playlist))
+    #     print(apple_music_creator.create(playlist))
