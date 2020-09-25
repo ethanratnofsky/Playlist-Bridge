1e3f8900cc5c2c04be4f53789fae78992b60a996
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-09-24 22:10:20 -0400

    restarting backend from scratch

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
