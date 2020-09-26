1461722481edba8d96a334b15b16b3036132df1d
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-09-26 00:44:46 -0400

    added temporary body to submit function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,8 +8,11 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/submit')
+@app.route('/submit', methods=['POST'])
 def submit():
+    print('Source Service: ', request.form['src_service'])
+    print('Destination Service: ', request.form['dest_service'])
+    print('Playlist URL: ', request.form['playlist_url'])
     return 'doing things!'
 
 
