1cc06bc36738b94df4996cf111abe5cd08d4157e
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-09-27 18:11:49 -0400

    removed unused conditions

--- api/bridger.py
+++ api/bridger.py
@@ -4,12 +4,6 @@ from .playlist_parsers import tidal_parser
 def bridge(src: str, dest: str, playlist_url: str):
     if src == 'tidal':
         playlist = tidal_parser.parse(playlist_url)
-    # elif src == 'spotify':
-    #     playlist = spotify_parser.parse(playlist_url)
-    # elif src == 'apple_music':
-    #     playlist = apple_music_parser.parse(playlist_url)
     #
     # if dest == 'spotify':
     #     print(spotify_creator.create(playlist))
-    # elif dest == 'apple_music':
-    #     print(apple_music_creator.create(playlist))
