fb5e166e9a3f5ee49ddbb2395924f1aff1371313
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-09-27 19:06:15 -0400

    bridge returns Playlist object

--- api/bridger.py
+++ api/bridger.py
@@ -1,9 +1,13 @@
+from .classes import Playlist
 from .playlist_parsers import tidal_parser
 
 
 def bridge(src: str, dest: str, playlist_url: str):
+    playlist = Playlist()
     if src == 'tidal':
         playlist = tidal_parser.parse(playlist_url)
     #
     # if dest == 'spotify':
     #     print(spotify_creator.create(playlist))
+
+    return playlist
