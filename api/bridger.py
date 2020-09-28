9208da205d3bb6bd6708e2a2bd6d497fb9c10a0d
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-09-27 20:13:51 -0400

    implement error handling for bridge parameters

--- api/bridger.py
+++ api/bridger.py
@@ -1,11 +1,18 @@
 from .classes import Playlist
 from .playlist_parsers import tidal_parser
+from flask import abort
 
 
 def bridge(src: str, dest: str, playlist_url: str):
     playlist = Playlist()
     if src == 'tidal':
         playlist = tidal_parser.parse(playlist_url)
+    else:
+        print('ERROR: Unknown source service.')  # TODO: Log this as an error
+        abort(500)  # 500 Internal Server Error
 
     if dest == 'spotify':
         return spotify_creator.create(playlist)
+    else:
+        print('ERROR: Unknown destination service.')  # TODO: Log this as an error
+        abort(500)  # 500 Internal Server Error
