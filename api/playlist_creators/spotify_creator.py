b7b218c52d28a49991b45f2fe681dbfe92ef8894
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-09-28 00:04:46 -0400

    implemented search_spotify() method

--- api/playlist_creators/spotify_creator.py
+++ api/playlist_creators/spotify_creator.py
@@ -3,11 +3,12 @@ from os import getenv
 import requests
 from flask import session
 
-from ..classes import Playlist, PlaylistCreatorResponse
+from ..classes import Playlist, PlaylistCreatorResponse, Song
 
 # Spotify API endpoints
 SPOTIFY_PROFILE_URL = getenv('SPOTIFY_PROFILE_URL')
 SPOTIFY_USER_PLAYLISTS_URL = getenv('SPOTIFY_USER_PLAYLISTS_URL')
+SPOTIFY_SEARCH_URL = getenv('SPOTIFY_SEARCH_URL')
 
 # Spotify access token
 ACCESS_TOKEN = session.get('spotify_tokens').get('access_token')
@@ -43,9 +44,55 @@ def create_playlist(user_id: str, playlist: Playlist) -> dict:
     return response.json()
 
 
-def create(playlist: Playlist) -> PlaylistCreatorResponse:
+def search_spotify(song: Song) -> dict:
+    # Parse song for title and primary artist
+    song_title = song.title
+    primary_artist = song.artists[0]
+
+    # Remove segment of song title that specifies featured artist(s)
+    # This is necessary because the 'featuring' segment can break the search query functionality
+    # TODO: This may be easier to program with RegEx
+    if ' (feat. ' in song_title:
+        song_title = song_title[:song_title.find(' (feat. ')] + song_title[song_title.find(')', song_title.find(
+            ' (feat. ')) + 1:]
+    elif ' [feat. ' in song_title:
+        song_title = song_title[:song_title.find(' [feat. ')] + song_title[song_title.find(']', song_title.find(
+            ' [feat. ')) + 1:]
+
+    # GET request header field
+    header = {
+        'Authorization': 'Bearer ' + ACCESS_TOKEN
+    }
+
+    # GET request query parameters
+    params = {
+        'q': song_title + ' ' + primary_artist,
+        'type': 'track',
+        'market': 'from_token',
+        'limit': 1
+    }
+
+    # Search Spotify with for song title and primary artist name with GET request to Spotify API endpoint
+    results = requests.get(SPOTIFY_SEARCH_URL, headers=header, params=params)
+
+    return results.json()
+
+
+def add_songs(songs: list, playlist_id: str):
+    uris = []  # List of Spotify URIs for songs in playlist
+    songs_not_found = []  # List of songs that are not found on Spotify
+
+    # Iterate through given songs; search Spotify for their URIs
+    for song in songs:
+        search_results = search_spotify(song)
+
+
+def create(src_playlist: Playlist) -> PlaylistCreatorResponse:
     # Get Spotify user ID
     user_id = get_user_id()
 
     # Create new Spotify playlist
-    spotify_playlist = create_playlist(user_id, playlist)
+    spotify_playlist = create_playlist(user_id, src_playlist)
+
+    # Add songs to new Spotify playlist
+    add_songs(src_playlist.songs, spotify_playlist.get('id'))
