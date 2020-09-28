45f3b96ee9648d9011d2ef81d0504a559684dd95
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-09-27 20:15:53 -0400

    added comments

--- api/bridger.py
+++ api/bridger.py
@@ -6,12 +6,15 @@ from flask import abort
 
 def bridge(src: str, dest: str, playlist_url: str) -> PlaylistCreatorResponse:
     playlist = Playlist()
+
+    # Parse playlist from source service
     if src == 'tidal':
         playlist = tidal_parser.parse(playlist_url)
     else:
         print('ERROR: Unknown source service.')  # TODO: Log this as an error
         abort(500)  # 500 Internal Server Error
 
+    # Return playlist created with destination service
     if dest == 'spotify':
         return spotify_creator.create(playlist)
     else:
