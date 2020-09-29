d9fa8e78d2015d672bbc5e81d432832ab7b2af43
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-09-29 00:00:13 -0400

    201 status codes should be the only successful codes for creating a playlist and adding songs

--- api/playlist_creators/spotify_creator.py
+++ api/playlist_creators/spotify_creator.py
@@ -48,7 +48,7 @@ def create_playlist(user_id: str, playlist: Playlist) -> dict:
     response = requests.post(SPOTIFY_USER_PLAYLISTS_URL.format(user_id=user_id), headers=headers, data=json.dumps(payload))
 
     # Check for non-success status code
-    if response.status_code != 200 and response.status_code != 201:
+    if response.status_code != 201:
         print('ERROR: Could not create a new Spotify playlist.')  # TODO: Log this as an error
         abort(response.status_code)
 
@@ -120,7 +120,7 @@ def add_songs(songs: list, playlist_id: str) -> PlaylistCreatorResponse:
     response = requests.post(SPOTIFY_ADD_SONGS_URL.format(playlist_id=playlist_id), headers=headers, data=payload)
 
     # Check for non-success status code
-    if response.status_code != 200 and response.status_code != 201:
+    if response.status_code != 201:
         print('ERROR: Could not add songs to Spotify playlist.')  # TODO: Log this as an error
         abort(response.status_code)
 
