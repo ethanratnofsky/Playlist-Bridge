3f849dc9fec6014ff1c5bc35d5b0ddfc50b93b29
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-10-03 14:25:00 -0400

    fixed bug where source and destination services were not defined in the playlist object

--- api/bridger.py
+++ api/bridger.py
@@ -7,18 +7,18 @@ from .playlist_parsers import tidal_parser
 
 def bridge(src: str, dest: str, playlist_url: str) -> PlaylistCreatorResponse:
     playlist = Playlist()
-    playlist.src_service = src
-    playlist.dest_service = dest
 
     # Parse playlist from source service
     if src == 'TIDAL':
         playlist = tidal_parser.parse(playlist_url)
+        playlist.src_service = src
     else:
         print('ERROR: Unknown source service.')  # TODO: Log this as an error
         abort(500)  # 500 Internal Server Error
 
     # Return playlist created with destination service
     if dest == 'Spotify':
+        playlist.dest_service = dest
         return spotify_creator.create(playlist, session.get('spotify_tokens'))
     else:
         print('ERROR: Unknown destination service.')  # TODO: Log this as an error
